833bd8454b40cac5f85a410baf60217757fb9b28
Sergey Aksenov <asm96@users.noreply.github.com>
2022-09-12 19:52:11 +0300

    Fix more bugs in player

--- src/scripts/player.js
+++ src/scripts/player.js
@@ -7,45 +7,76 @@
   const soundRangeButton = $('.player__sound-range-button');
   const durationEstimate = $('.player__duration-estimate');
   const durationCompleted = $('.player__duration-completed');
+  const updateRange = 1000;
   let currentSoundButtonPosition = '';
   let currentVolume = 0;
+  let interval;
+
+  const formatTime = timeSec => {
+    const roundTime = Math.round(timeSec);
+    const minutes = addZero(Math.floor(roundTime / 60));
+    const seconds = addZero(roundTime % 60);
+
+    function addZero(num) {
+      return num < 10 ? `0${num}` : num;
+    }
+
+    return `${minutes}:${seconds}`;
+  };
+
+  const updateDuration = () => {
+    const durationSec = player.duration;
+    const completedSec = player.currentTime;
+    const completedPercent = (completedSec / durationSec) * 100;
+
+    playbackButton.css('left', `${completedPercent}%`);
+    durationCompleted.text(formatTime(completedSec));
+  };
+
+  const playStop = () => {
+    if (player.paused) {
+      playerContainer.addClass('active');
+      interval = setInterval(updateDuration, updateRange);
+      player.play();
+    } else {
+      playerContainer.removeClass('active');
+      clearInterval(interval);
+      player.pause();
+    }
+  };
 
   $('.player__start').on('click', e => {
     e.preventDefault();
-    if (playerContainer.hasClass('active')) {
-      playerContainer.removeClass("active");
-      player.pause();
-    } else {
-      playerContainer.addClass("active");
-      player.play();
-    }
+    playStop();
   });
 
-  $('.player__splash-button').on('click', () => {
-    playerContainer.addClass("active");
-    player.play();
-  });
+  $('.player__splash-button').on('click', playStop);
 
   $('.player__playback').on('click', e => {
     const bar = $(e.currentTarget);
-    const clickedPosition = e.originalEvent.layerX;
+    const clickedPosition = e.originalEvent.layerX >= 0 ? e.originalEvent.layerX : 0;
     const newButtonPositionPersent = (clickedPosition / bar.width()) * 100;
     const newPlaybackPositionSec = (player.duration / 100) * newButtonPositionPersent;
 
     playbackButton.css('left', `${newButtonPositionPersent}%`);
     player.currentTime = newPlaybackPositionSec;
+    playStop();
   });
 
   $('.player__sound-range').on('click', e => {
     const bar = $(e.currentTarget);
-    const clickedPosition = e.originalEvent.layerX;
-    const soundLevel = clickedPosition / bar.width();
+    const clickedPosition = e.originalEvent.layerX >= 0 ? e.originalEvent.layerX.toFixed() : 0;
+    const soundLevel = (clickedPosition / bar.width()).toPrecision(1);
+    const soundBtn = $('.player__sound');
 
     soundRangeButton.css('left', `${clickedPosition}px`);
     player.volume = soundLevel;
+
+    soundLevel <= 0.05 ? soundBtn.addClass('muted') : soundBtn.removeClass('muted');
   });
 
   $('.player__sound').on('click', e => {
+    e.preventDefault();
     const $this = $(e.currentTarget);
 
     if ($this.hasClass('muted')) {
@@ -61,47 +92,13 @@
     }
   });
 
-  const formatTime = timeSec => {
-    const roundTime = Math.round(timeSec);
-    const minutes = addZero(Math.floor(roundTime / 60));
-    const seconds = addZero(roundTime % 60);
-
-    function addZero(num) {
-      return num < 10 ? `0${num}` : num;
-    }
-
-    return `${minutes} : ${seconds}`;
-  };
-
   player.addEventListener('canplay', () => {
-    let interval;
     const durationSec = player.duration;
 
     durationEstimate.text(formatTime(durationSec));
-
-    if (typeof interval !== undefined) {
-      clearInterval(interval);
-    }
-
-    interval = setInterval(() => {
-      const completedSec = player.currentTime;
-      const completedPercent = (completedSec / durationSec) * 100;
-
-      playbackButton.css('left', `${completedPercent}%`);
-
-      durationCompleted.text(formatTime(completedSec));
-    }, 1000);
   });
 
   player.addEventListener('click', e => {
-    if (e.currentTarget) {
-      if (playerContainer.hasClass('active')) {
-        playerContainer.removeClass("active");
-        player.pause();
-      } else {
-        playerContainer.addClass("active");
-        player.play();
-      }
-    }
-  })
+    if (e.currentTarget) playStop();
+  });
 })()
